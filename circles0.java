a9261e97fb35ca1278aae4e6e966b3aac5c6e154
acosj47 <justinacostaxi@hotmail.com>
2015-09-02 20:49:43 -0400

    made another change

--- circles0.java
+++ circles0.java
@@ -2,11 +2,13 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 300);                // Try changing the width & height.
+  size(200, 400);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 60,30 );       // Try changing the (x,y) and size.
-text( "Ha ha!", 50, height- 50);
+  ellipse( 60,50, 60,30 );       // Try changing the (x,y) and size.
+text( "He he!", 50, height- 50);
 }
+
+// comment
