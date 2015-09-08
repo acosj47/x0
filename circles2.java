ec3e81ea47a02329eb1d5c0cce6e87384bd8a0a2
acosj47 <justinacostaxi@hotmail.com>
2015-09-08 11:47:30 -0400

    Added name and Body

--- circles2.java
+++ circles2.java
@@ -2,6 +2,7 @@
 String title=  "Click for another circle.";
 String subtitle=  "Press any key to erase.";
 String author=  "Bruce Alan Martin";
+String name= "Justin";
 
 // GLOBAL VARIABLES //
 float x,y;
@@ -9,21 +10,55 @@ int h,w;
 
 // Setup:  screen size, initialization. //
 void setup() {
+  background(0, 0, 150);
+  
   size(500, 500);
   x=  width/2;
   y=  height/2;
-  w=  80;          // width of ellipse.
-  h=  80;          // height of ellipse
+  w=  150;          // width of ellipse.
+  h=  150;          // height of ellipse
+  
+  
+
+  
+
+  
+  
+  
+  
+  
+
 }
 
+
 // Next frame. //
-void draw() {
-  text( title, 10, 10 );
+void draw(){
+  
+  text( title, 10, 10 ); 
   text( subtitle, width/2, 10 );
   text( author, 10, height-10 );
   text( w+"x"+h, width/2,height-10 );
+  
+  
+
+ 
   //
+  stroke(0);
+  fill(0, 150, 0);
   ellipse(x, y, w, h);
+  fill(0, 150, 0);
+  rect(175, 300, 150, 150);
+  fill(0);
+  ellipse(210, 230, 40, 20);
+  ellipse(280, 230, 40, 20);
+  line(175, 300, 150, 350);
+  line(320, 300, 350, 350);
+  fill(255);
+  text(name, 230, 370);
+  
+
+  
+  
 }
 
 
@@ -37,7 +72,7 @@ void mousePressed() {
 //  Handle keyboard //
 void keyPressed() {
   // Erase everything, change size & color. //
-  background( random(255), random(255), random(255) );
+  background( 255, 0, 0 );
   fill( random(255), random(255), random(255) );
   w=  int( random(50,150) );
   h=  int( random(50,150) );
